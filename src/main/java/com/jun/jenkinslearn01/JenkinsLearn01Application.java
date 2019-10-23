217f7461dd3325b8ab65de13dee8fb901df9b52b
jun <156750703@qq.com>
2019-10-23 09:29:43 +0800

    init

---
+++ src/main/java/com/jun/jenkinslearn01/JenkinsLearn01Application.java
@@ -0,0 +1,13 @@
+package com.jun.jenkinslearn01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLearn01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsLearn01Application.class, args);
+    }
+
+}
